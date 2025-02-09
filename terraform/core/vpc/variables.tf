ba882b1dbec2119621912afcc73e8ffea646bb51
Reza Zerehpoosh <ironcladgeek@hotmail.com>
2025-02-09 23:09:32 +0100

    chore: Remove default values from VPC variable definitions

--- terraform/core/vpc/variables.tf
+++ terraform/core/vpc/variables.tf
@@ -11,17 +11,14 @@ variable "environment" {
 variable "region" {
   description = "Aws region"
   type        = string
-  default     = "eu-north-1"
 }
 
 variable "vpc_cidr" {
   description = "CIDR block for the VPC"
   type        = string
-  default     = "10.0.0.0/16"
 }
 
 variable "availability_zones" {
   description = "List of availability zones"
   type        = list(string)
-  default     = ["eu-north-1a", "eu-north-1b", "eu-north-1c"]
 }
